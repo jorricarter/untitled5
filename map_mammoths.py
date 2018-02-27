1cc64dee7b1e438ce2c28e7678f7d9e883ca5ef0
Jorri <Carter>
2018-02-27 15:08:42 -0600

    complete. trying to remove lib from commits

--- map_mammoths.py
+++ map_mammoths.py
@@ -2,9 +2,13 @@ import folium
 from folium import plugins
 import csv
 
-mammoth_color = ['blue']
+mammoth_colors = {'Mammuthus columbi' : 'green',
+ 'Mammuthus primigenius': 'blue',
+ 'Mammuthus hayi' : 'purple',
+ 'Mammuthus exilis' : 'red',
+ 'Mammuthus': 'orange'}
 
-mammoth_map = folium.Map(location=[40, -120], zoom_start=3, tiles='Stament Terrain')
+mammoth_map = folium.Map(location=[40, -120], zoom_start=3, tiles='Stamen Terrain')
 
 lat_lng = []
 
@@ -18,12 +22,11 @@ with open('mammoth_data.csv', 'r') as mammoth_csv:
         marker_text = '%s found in %s, %s. %s.' % (line[0], line[6], line[5], line[7])
         if line[1]:
             marker_text += ' %s %s ' % (line[1], line[2])
-        marker = folium.Marker([lat, lon], popup=marker_text)
-        color = mammoth_color
+        color = mammoth_colors[line[0]]
         marker = folium.Marker([lat, lon], popup=marker_text, icon=folium.Icon(color=color))
         marker.add_to(mammoth_map)
 mammoth_map.save('mammoth_map.html')
 
 heatmap = folium.Map(location=[40, -120], zoom_start=3)
 heatmap.add_children(plugins.HeatMap(lat_lng))
-heatmap.save('mammoth_heatmap.htmml')
+heatmap.save('mammoth_heatmap.html')
